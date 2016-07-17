10ecb7f7519adddc4c1b271479cae7b2a47d25a1
phasedchirp <sean.martin@nyu.edu>
2016-07-17 13:20:37 -0400

    Added functor instance and tests, cleaned up a few things

--- pqueue.hs
+++ pqueue.hs
@@ -1,4 +1,39 @@
+-- Written by Annie Cherkaev and Sean Martin
+
 import Test.QuickCheck
+
+
+data SkewHeap a = Empty | Node (SkewHeap a) a (SkewHeap a) deriving (Eq,Show)
+
+singleton :: Ord a => a -> SkewHeap a
+singleton x = insert x Empty
+
+insert :: Ord a => a -> SkewHeap a -> SkewHeap a
+insert x h = merge (Node Empty x Empty) h
+
+
+merge :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
+merge Empty h = h
+merge h Empty = h
+merge h1@(Node l1 v1 r1) h2@(Node l2 v2 r2) | v1 < v2 = Node (merge r1 h2) v1 l1
+                                            | otherwise = Node (merge r2 h1) v2 l2
+
+
+maxDepth :: SkewHeap a -> Int
+maxDepth Empty = 0
+maxDepth (Node l _ r) = 1 +  max (maxDepth l) (maxDepth r)
+
+minDepth :: SkewHeap a -> Int
+minDepth Empty = 0
+minDepth (Node l _ r) = 1 +  min (minDepth l) (minDepth r)
+
+-- Typeclass instances:
+
+instance Functor SkewHeap where
+  fmap _ Empty = Empty
+  fmap f (Node Empty x Empty) = Node Empty (f x) Empty
+  fmap f (Node l x r) = Node (fmap f l) (f x) (fmap f r)
+
 
 instance (Ord a, Arbitrary a) => Arbitrary (SkewHeap a) where
   arbitrary = sized arbitrarySkewHeap
@@ -11,19 +46,26 @@ arbitrarySkewHeap size = do
   ts <- arbitrarySkewHeap newSize
   return $ merge (Node Empty t Empty) ts
 
-depth :: SkewHeap a -> Int
-depth Empty = 0
-depth (Node l _ r) = 1 + (depth l)
 
--- printHeap :: Show a => SkewHeap a -> String
--- printHeap Empty = ""
--- printHeap (Node Empty x Empty) = show x
--- printHeap h = print' h []
---   where print' (Node l v r) acc=
 
--- print' :: Show a => SkewHeap a -> [[a]] -> [[a]]
--- print' h@(Node l v r) acc =  (v : (head acc)) : (print' l) ++ (print' r)  : acc
--- print' Empty acc = acc
+-- Testing Functor laws (not sure these are totally correct tests?):
+
+prop_functorID :: Eq a => SkewHeap a -> Bool
+prop_functorID h = fmap id h == h
+
+prop_functorComposition :: Eq c => (b -> c) -> (a -> b) -> SkewHeap a -> Bool
+prop_functorComposition f g h = (fmap (f . g) h) == (fmap f (fmap g h))
+
+
+ -- Utility functions:
+
+fromInt :: Int -> SkewHeap Int
+fromInt n = fromInt' n (abs n)
+ where fromInt' c d | d <= 0 = Node Empty c Empty
+                    | d > 0 = Node (fromInt' (c+1) (d-1)) c (fromInt' (c+1) (d-1))
+
+fromList :: (Ord a, Arbitrary a) => [a] -> SkewHeap a
+fromList (x:xs) = foldr insert (singleton x) xs
 
 -- printing solution from:
 -- http://stackoverflow.com/questions/12556469/nicely-printing-showing-a-binary-tree-in-haskell
@@ -35,28 +77,5 @@ prettyprint_helper (Node left node right) = (show node) : (prettyprint_subtree l
               prettyprint_subtree left right = ((pad "+- " "|  ") (prettyprint_helper right)) ++ ((pad "`- " "   ") (prettyprint_helper left))
 prettyprint_helper Empty = []
 
-
-
--- fromInt :: (Ord a, Arbitrary a) => Int -> SkewHeap a
-
--- fromList :: (Ord a, Arbitrary a) => [a] -> SkewHeap a
-
-data SkewHeap a = Empty | Node (SkewHeap a) a (SkewHeap a) deriving (Eq,Show)
-
-singleton :: Ord a => a -> SkewHeap a
-singleton x = insert x Empty
-
-insert :: Ord a => a -> SkewHeap a -> SkewHeap a
--- insert x Empty = Node Empty x Empty
-insert x h = merge (Node Empty x Empty) h
-
-
-merge :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
-merge Empty h = h
-merge h Empty = h
-merge h1@(Node l1 v1 r1) h2@(Node l2 v2 r2) | v1 < v2 = Node (merge r1 h2) v1 l1
-                                            | otherwise = Node (merge r2 h1) v2 l2
-
-
--- test = Node Empty 3 (Node Empty 5 Empty)
--- test' = Node (Node Empty 20 Empty) 1 Empty
+printHeap :: Show a => SkewHeap a -> IO ()
+printHeap = putStrLn . prettyprint
